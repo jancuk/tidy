e6be665f6aac2c478564ae4c485dfe3041547646
azhar <azhar.amirr@gmail.com>
2026-05-17 21:20:39 +0700

    init repo

---
+++ Tidy/TidyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidyApp.swift
+//  Tidy
+//
+//  Created by Azhar Amir on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TidyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
