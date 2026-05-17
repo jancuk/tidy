1ae57723f44bffea4f9b7a9698e32f01928bd3d1
azhar <azhar.amir@amartha.com>
2026-05-17 21:20:39 +0700

    init repo

---
+++ TidyTests/TidyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TidyTests.swift
+//  TidyTests
+//
+//  Created by Azhar Amir on 17/05/26.
+//
+
+import Testing
+@testable import Tidy
+
+struct TidyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
